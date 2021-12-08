1c97e7af2f097f477e1365c0be28143b86cd94c6
elo1lson <eloilsonfontenele2@gmail.com>
2021-12-08 16:02:17 -0300

    reformulação de código

--- commands/add.js
+++ commands/add.js
@@ -2,18 +2,47 @@ const Discord = require("discord.js")
 const db = require("quick.db")
 
 module.exports.run = async (client, message, args) => {
+  //Variáveis Globais
+  
+	var user = message.author.id
+	var membro = message.mentions.users.first()
+	var mone = await db.fetch(`bank.{user}.money`)
+	var autor = message.author.id
+	var tomori = 539945189901336586
+	var marcacao = args[0] == membro
 	
-	user = message.author.id
-	membro = message.mentions.users.first()
-  mone = await db.fetch(`bank.{user}.money`)
-		
-	if (!membro){
-		membro = message.author
-		mone = await db.fetch(`bank.{user}.money`)
-		db.add(`bank.${user}.money`, `${args[0]}`)
-		
-	}else{
-		db.add(`bank.${membro}.money`,`10000`)
-		message.reply(`Foi adicionado 10000 moedas a ${membro}`)
+	//Embeds
+	
+	var embedDev = new Discord.MessageEmbed()
+		.setColor('GREEN')
+		.setDescription(`Seu saldo agora é: ${balance}`);
+	var embed = new Discord.MessageEmbed()
+		.setColor('GREEN')
+		.setDescription(`${message.author} foi adicionado ${args[1]} moedas para ${membro}`)
+		.addField(`Agora ${membro.username} tem:`, `${balance} moedas`)
+  var embedErro = new Discord.MessageEmbed()
+  	.setColor('GREEN')	
+  	.setDescription(`<:no:915054924725882892> | ${message.author.username} você não tem permissão pra fazer isso!`)
+	
+	//Tomando decisões com IF
+	
+	if(autor == tomori){
+		if(membro){
+			var bank = await db.fetch(`bank`)
+			var check = await db.fetch(`bank.${membro.id}`);
+			var add = db.add(`bank.${membro.id}.money`, `${args[1]}`)
+			var balance = await db.fetch(`bank.${membro.id}.money`);
+			message.reply({ embeds: [embed] })
+		}else{
+			var check = await db.fetch(`bank.${user}`);
+			var add = db.add(`bank.${user}.money`,`${args[1]}`)
+			var balance = await db.fetch(`bank.${user}.money`);
+			return message.reply({embeds: [embedDev]})
+		}
+	}
+	//Mensagem que será Retornada caso if retorne falso
+	else{
+		return message.reply({embeds:[embedErro]})
 	}
 }
+//Finalizado em 08/12/21
--- commands/avatar.js
+++ commands/avatar.js
@@ -1,26 +1,20 @@
 const Discord = require('discord.js');
-const cor = require("chalk")
-var path = require('path');
-var cmd = path.basename(__filename);
+
 module.exports.run = async (client, message, args, prefix) => {
-for (var i = 0; i < 2; i++) {
-   console.log(i);
-   // more statements
-}
-  const embd = new Discord.MessageEmbed()
-  .setTitle(`<a:HYpositive:763111725510950932> | Comando: ${prefix}avatar`)
-  .setColor("RANDOM")
-  .setDescription(`<a:HYseta1:756119648654852106> **Use**: ${prefix}avatar <usuário>`)
-  .addField(`👍 | Observação:`, `<a:HYseta1:756119648654852106> Nenhuma`)
-  .setFooter(`© HypedGroupCode`);
-
-  let membro = message.mentions.users.first()
-  if(!membro) membro = message.author
-  const embed = new Discord.MessageEmbed()
-  .setColor("RANDOM")
+  //Embed 
+  var embed = new Discord.MessageEmbed()
+  .setColor('GREEN')
   .setImage(membro.displayAvatarURL({ dynamic: true, format: "png", size: 1024 }))
-  .setDescription(`baixe essa imagem clicando [aqui](${membro.displayAvatarURL({ dynamic: true, format: "png", size: 1024 })})`)
-
-  message.channel.send({embeds:[embed]});
-  console.log(cor.blue(`${message.author.username} usou ${cmd}`))
+  .setDescription(`**Baixe essa imagem clicando [aqui](${membro.displayAvatarURL({ dynamic: true, format: "png", size: 1024 })})**`)
+  
+  //Embed de erro  
+  var embedErro = new Discord.MessageEmbed()
+  	.setDescription(`${message.author} use \`\`${prefix}\`\`avatar <@usuario> para avatar de alguém, ou \`\`${prefix}\`\`avatar para seu avatar}`)
+  var membro = message.mentions.users.first()
+  if(!membro) membro = message.author
+  if(args[0] != membro){
+  	message.reply({embeds:[embedErro]})
+  }
+  message.reply({embeds:[embed]});
 }
+//Finalizado em 08/12/21
