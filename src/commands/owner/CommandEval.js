5a0b41a0a013a7f2cd48ca18a2a7e92d3b08993d
elo1lson <eloilsonfontenele2@gmail.com>
2022-02-08 08:39:46 -0300

    eval

--- src/commands/owner/CommandEval.js
+++ src/commands/owner/CommandEval.js
@@ -1,4 +1,3 @@
-
 const Discord = require('discord.js')
 const { MessageActionRow, MessageSelectMenu } = require('discord.js');
 const Command = require('../../structures/command/command.js')
@@ -10,35 +9,33 @@ module.exports = new Command({
 		if (!args[0]) {
 			return message.channel.send(`**|** ${message.author}, insira um valor para executar o eval.`);
 		}
-		await message.channel.send(`**|** Executando...`)
-			try {
-				let beforeRunning = Date.now(); // Define a data de execução
-				let result = eval(args.join(' ')); // Gera os argumentos do eval
-		//		message.reply({content: '```js\n' + result + '```'})
-				if (result instanceof Promise) {
-					message.edit('O código retornou uma promise - aguardando ela ser resolvida...')
-					await result
-				}; // Se retorna Promise, ele enviará o recado
+		try {
+			let beforeRunning = Date.now(); // Define a data de execução
+			let result = eval(args.join(' ')); // Gera os argumentos do eval
+			//		message.reply({content: '```js\n' + result + '```'})
+			if (result instanceof Promise) {
+				message.edit('O código retornou uma promise - aguardando ela ser resolvida...')
+				await result
+			}; // Se retorna Promise, ele enviará o recado
 
-				if (typeof result !== 'string') result = require('util').inspect(result); // Se retornar uma string, ele enviará o recado
-				let end = (Date.now() - beforeRunning); // Define o final do tempo
+			if (typeof result !== 'string') result = require('util').inspect(result); // Se retornar uma string, ele enviará o recado
+			let end = (Date.now() - beforeRunning); // Define o final do tempo
 
-				let embed = new Discord.MessageEmbed()
-					.setTimestamp()
-					.setColor(cor)
-					.addField('📩 Entrada', `\`\`\`js\n${args.join(" ")}\`\`\``)
-					.addField('🚩 Saída', `\`\`\`js\n${result.slice(0, 1010)}\n\`\`\``)
+			let embed = new Discord.MessageEmbed()
+				.setTimestamp()
+				.setColor(cor)
+				.addField('📩 Entrada', `\`\`\`js\n${args.join(" ")}\`\`\``)
+				.addField('🚩 Saída', `\`\`\`js\n${result.slice(0, 1010)}\n\`\`\``)
 
-				if (result.length > 1010) embed.addField('🚩 Continuação do Resultado', `\`\`\`js\n${result.slice(1010, 2020)}\n\`\`\``); // Se o eval for maior que os 1010 de caracteris, ele adicionará um field
+			if (result.length > 1010) embed.addField('🚩 Continuação do Resultado', `\`\`\`js\n${result.slice(1010, 2020)}\n\`\`\``); // Se o eval for maior que os 1010 de caracteris, ele adicionará um field
 
-				message.edit({embeds: embed }); // Notificará ao usuário sobe o eval
-			} catch (e) {
-				let embed = new Discord.MessageEmbed()
-					.setTimestamp()
-					.setDescription('```js\n' + e.stack.slice(0, 2000) + '```')
-					.setColor(cor)
-				message.edit({embeds: embed}); // Notificará ao usuário sobre o erro no eval
-			}; // Caso ocorra um erro, ele irá retornar essa ação
+			message.reply({ embeds: embed }); // Notificará ao usuário sobe o eval
+		} catch (e) {
+			let embed = new Discord.MessageEmbed()
+				.setTimestamp()
+				.setDescription('```js\n' + e.stack.slice(0, 2000) + '```')
+				.setColor(cor)
+			message.reply({ embeds: embed }); // Notificará ao usuário sobre o erro no eval
+		}; // Caso ocorra um erro, ele irá retornar essa ação
 	} // Executa p código do comando // Exporta o comando com todas as configurações e informações
 })
-
