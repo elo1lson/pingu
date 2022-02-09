4050d19fdd41d717c6f444cdba064ac09a5e86d0
elo1lson <eloilsonfontenele2@gmail.com>
2022-02-09 12:24:27 -0300

    Kiss

--- src/commands/fun/CommandKiss.js
+++ src/commands/fun/CommandKiss.js
@@ -12,18 +12,34 @@ module.exports = new Command({
 		ob: '<usuario>'
 	},
 	author: 'tomori',
-	run: async (client, message, args, prefix) => {
+	run: async (client, message, args, prefix, cor) => {
 		let mencao = message.mentions.users.first()
+		if(!mencao){
+			return message.reply({content: 'Marque alguem para fazer kiss kiss'})
+		}
+		if(mencao == client.user){
+			return message.reply({content: '☺ Sou casada sabia? Não posso aceitar isso'})
+		}
+		if(mencao == message.author){
+			message.reply({content: 'Esquizofrênia amigo(a)?'})
+		}
 		let array = [
-
+			"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK5isPRtOdV5YvaW9bpeZVv1HuoQqKzgMmqQ&usqp=CAU",
+			"https://acegif.com/wp-content/uploads/anime-kiss-m.gif",
+			"https://animesher.com/orig/1/167/1673/16736/animesher.com_gif-couple-kiss-1673657.gif",
+			"https://animesher.com/orig/1/123/1235/12354/animesher.com_anime-boy-anime-couple-shin-1235427.gif",
+			"https://animesher.com/orig/1/140/1404/14046/animesher.com_gif-manga-kissing-1404608.gif"
 			]
+		let i = Math.floor(Math.random() * array.length)
 		let u = message.author
 		let embed = new Embed(u)
+		.setColor(cor)
 		.setDescription(`${message.author} fez Kiss Kiss`)
+		.setImage(`${array[i]}`)
 		try {
 			message.reply({ content: [embed] })
 		} catch (e) {
-			console.log('Erro no comando Hug: ' + e)
+			console.log('Erro no comando Kiss: ' + e)
 			message.reply({ content: '❌ Ocorreu um erro ao tentar executar esse comando' })
 		}
 	}
