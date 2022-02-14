0d8a2432ff7df2ef8d8615e7918c3bd3bf39253e
elo1lson <eloilsonfontenele2@gmail.com>
2022-02-14 20:32:30 -0300

    001

--- src/commands/bot/CommandBotinfo.js
+++ src/commands/bot/CommandBotinfo.js
@@ -32,7 +32,7 @@ module.exports = new Command({
 		let notsend = new Embed(user)
 			.setTitle('🐊 Sobre')
 			.setThumbnail(`${client.user.avatarURL({dinamyc: true})}`)
-			.setDescription(`Sou a <:toto:934042191414833173> ${client.user.username}, estou tentando trazer alegria para todos com meus ${c} comandos,	 atualmente eu faço parte de mais de **${client.guilds.cache.size}** servidores, e cuido de **${client.users.cache.size}** lindas pessoas, e já  executei **${Geral}** comandos desde que fui criada!\n\nFui desenolvida em <:js:935134807631999006>** [JavaScript](https://g.co/kgs/fiDGbJ)** utilizando <:djs:935139154340237322> **[Discord.js](https://github.com/discordjs/discord.js)**, e sou totalmente <:open_source:935141228889124875> **Open Source**! Se você quiser ver meu código fonte ou contribuir para a minha criação, clique [aqui](https://github.com/elo1lson/Open-Os_Bot)`)
+			.setDescription(client.lang.BOTINFO.embed.description)
 			.addFields(
 				{
 					name: "Criador",
