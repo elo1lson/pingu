e51f6ac599dd20219b85ba1eda82bec3fdc9dd3d
elo1lson <eloilsonfontenele2@gmail.com>
2022-02-14 22:15:06 -0300

    Comando botinfo atualizado para tradução em breve

--- src/commands/bot/CommandBotinfo.js
+++ src/commands/bot/CommandBotinfo.js
@@ -29,38 +29,41 @@ module.exports = new Command({
 		let comp = newlib.length
 		let newcomp = comp - 28
 		let send = client.lang.BOTINFO.embed.description.replace("%BOTNAME%", client.user.username)
+		//let fieldtwovalue = client.lang.BOTINFO.embed.fieldtwo.value
+
 		send = send.replace("%COMMANDS%", "16")
 		send = send.replace("%GUILDS%", client.guilds.cache.size)
 		send = send.replace("%USERS%", client.users.cache.size)
 		send = send.replace("%TOTALCOMMANDS%", "999")
 		send = send.replace("%JS%", "[JavaScript](https://pt.m.wikipedia.org/wiki/JavaScript)")
-		send = send.replace("%DJS%", "[Discord.js]{https://github.com/discordjs/discord.js}")
-		send = send.replace("%OPENSOURCE%	", "[Open Source]{https://www.google.com/search?q=open+source&oq=open+so&aqs=chrome.4.69i57j69i61j69i60l2j0i433i512j46i199i433i465i512j69i59j0i512.3507j0j4&client=ms-android-samsung-gj-rev1&sourceid=chrome-mobile&ie=UTF-8}")
-		send = send.replace("%LINK%", "[aqui]{https://github.com/tomoriOpen/Tomori}")
+		send = send.replace("%DJS%", "[Discord.js](https://github.com/discordjs/discord.js)")
+		send = send.replace("%OPENSOURCE%	", "[Open Source](https://www.google.com/search?q=open+source&oq=open+so&aqs=chrome.4.69i57j69i61j69i60l2j0i433i512j46i199i433i465i512j69i59j0i512.3507j0j4&client=ms-android-samsung-gj-rev1&sourceid=chrome-mobile&ie=UTF-8)")
+		send = send.replace("%LINK%", "[aqui](https://github.com/tomoriOpen/Tomori)")
 
 		let user = message.author
 		let notsend = new Embed(user)
-			.setTitle('🐊 Sobre')
+			.setTitle(client.lang.BOTINFO.embed.title)
 			.setThumbnail(`${client.user.avatarURL({dinamyc: true})}`)
 			.setDescription(send)
 			.addFields(
 				{
-					name: "Criador",
-					value: `\`\`bobycake#8894\`\``
+					name: client.lang.BOTINFO.embed.fieldone.name,
+					value: `\`\`${client.lang.BOTINFO.embed.fieldone.value
+					}\`\``
 				},
 				/*{
 					name: 'Bibliotecas Utilizadas na minha criação:',
 					value: `**${newlib.substr(0, newcomp)}**`
 				},*/
 				{
-					name: 'Links Úteis:',
+					name: client.lang.BOTINFO.embed.fieldtwo.name,
 					value: '<:github:935233776227856415> [Github](https://github.com/elo1lson/Open-Os_Bot)\n<:sac:935235170552909965> [Suporte](https://discord.gg/NAdSr57Few)\n<:dev:935237634270310421> [Developer](https://github.com/elo1lson)'
 				})
 		const row = new MessageActionRow()
 			.addComponents(
 				new MessageButton()
 				.setCustomId('primary')
-				.setLabel('Próximo')
+				.setLabel(client.lang.BOTINFO.button.one)
 				.setStyle('PRIMARY')
 				.setDisabled(true)
 			);
