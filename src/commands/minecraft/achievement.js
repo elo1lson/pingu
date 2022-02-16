d4c4d0f288ca68d84a2e40d477cd78f3db1d7f64
elo1lson <eloilsonfontenele2@gmail.com>
2022-02-16 10:10:52 -0300

    +erros

--- src/commands/minecraft/achievement.js
+++ src/commands/minecraft/achievement.js
@@ -1,28 +1,48 @@
-const snekfetch = require('snekfetch');
-
 const Discord = require('discord.js')
-const { MessageActionRow, MessageButton } = require('discord.js');
 const Command = require('../../structures/command/command.js')
 const Embed = require('../../structures/client/ClientEmbed.js')
+const snekfetch = require('snekfetch');
 module.exports = new Command({
-	name: 'cat',
-	description: "client.lang.examples.CAT.description",
-	category: 'Fun',
-	aliases: ['gato'],
+	category: 'Minecraft',
+	name: 'achievement',
+	description: 'Envia uma imagem com uma conquista do minecraft',
+	aliases: ['conquista'],
 	usage: {
-		ob: "none",
-		op: "none"
+		op: 'none',
+		ob: '<texto>'
 	},
 	author: 'tomori',
-	run: async (client, message, args) => {
-		let url = `http://aws.random.cat/meow`
-		let bod = snekfetch.get(url).then(r => {
-			//	let { body } = await superagent.get(`http://aws.random.cat/meow`);
-			let u = message.author
-			let catembed = new Embed(u)
-				.setTitle("Gatos 🐱")
-				.setImage(r.body);
-			message.channel.send({ embeds: [catembed] });
-		})
+	run: async (client, message, args, prefix) => {
+		let u = message.author
+		let not = new Embed(u)
+			.setTitle('❔ Como usar?')
+			.setDescription(`\`\`achievement\`\` => Envia uma imagem com uma conquista do minecraft`)
+			.addFields(
+			{
+				name: ':books: Exemplos:',
+				value: `\`\`${prefix}$achievement\`\` Eu amo a ${client.user.username}\n\`\`${prefix}$achievement\`\` Eu sou legal`
+			},
+			{
+				name: ':twisted_rightwards_arrows: Aliases:',
+				value: `\`\`conquista\`\``
+			})
+		if (!args.join(" ")) return message.reply({ embeds: [not] })
+		let [title, contents] = args.join(" ").split("|");
+		if (!contents)[title, contents] = ["Conquista desbloqueada!", title];
+		let rnd = Math.floor((Math.random() * 39) + 1);
+
+		if (args.join(" ").toLowerCase().includes("burn")) rnd = 38;
+		if (args.join(" ").toLowerCase().includes("cookie")) rnd = 21;
+		if (args.join(" ").toLowerCase().includes("cake")) rnd = 10;
+		if (title.length > 24 || contents.length > 22) return message.channel.send("Você inseriu mais de 22 caracteres.");
+		const url = `https://www.minecraftskinstealer.com/achievement/a.php?i=${rnd}&h=${encodeURIComponent(title)}&t=${encodeURIComponent(contents)}`;
+
+		try {
+			snekfetch.get(url).then(r =>{
+				message.channel.send({ files: [{ attachment: r.body }] })});
+		} catch (e) {
+			console.log('Erro no comando Achievement: ' + e)
+			message.reply({ content: '❌ Ocorreu um erro ao tentar executar esse comando' })
+		}
 	}
 })
