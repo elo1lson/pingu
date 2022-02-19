1fa6f157198faea6650174a024486dfcf8ef86b6
elo1lson <eloilsonfontenele2@gmail.com>
2022-02-19 20:02:13 -0300

    Finalmente terminei o eval

--- src/commands/owner/CommandEval.js
+++ src/commands/owner/CommandEval.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js')
-const { MessageActionRow, MessageSelectMenu } = require('discord.js');
+const { MessageActionRow, MessageButton } = require('discord.js');
 const Command = require('../../structures/command/command.js')
+const Embed = require('../../structures/client/ClientEmbed.js')
 module.exports = new Command({
 	name: 'eval',
 	aliases: ['ev'],
@@ -8,40 +9,82 @@ module.exports = new Command({
 		if (message.author.id != process.env.KEY) {
 			return message.reply({ content: "Quer me fuder é seu filho da puta? Vai usar eval com a tua mãe", ephemeral: true })
 		}
-		if (!args[0]) {
-			return message.edit(`Insira um valor para executar o eval.`);
-		}
-		try {
-			let beforeRunning = Date.now(); // Define a data de execução
-			let result = eval(args.join(' ')); // Gera os argumentos do eval
-			if (result instanceof Promise) {
-				await message.edit('O código retornou uma promise - aguardando ela ser resolvida...')
-				await result
-			}
-			if (typeof result !== 'string') result = require('util').inspect(result);
+		var row = new MessageActionRow()
+			.addComponents(
+				new MessageButton()
+				.setCustomId('pc')
+				.setEmoji('💻')
+				.setStyle('SUCCESS')
+			)
+			.addComponents(
+				new MessageButton()
+				.setCustomId('lup')
+				.setEmoji('🔎')
+				.setStyle('SUCCESS')
+			)
+		let u = message.author
+		let stageone = new Embed(u)
+		stageone.setDescription('Escolha o modo de execução do codigo')
 
-			console.log(typeof result)
-			let u = message.author
-			let end = (Date.now() - beforeRunning); // Define o final do tempo
-			let embd = new Embed(u)
-			embd.addField("📥 Entrada", "```js\n" + args.join(' ') + "```")
-			embd.addField("📩 Saída", "```js\n" + result.slice(0, 1010) + "```")
-			embd.setFooter({
-				text: `\`\`${end}ms\`\``,
-				IconURL:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAY24-TSTqg2C9tlU2TpNpRnLW11kx8m4Mzw&usqp=CAU'
-			})
-			if (result.length > 1010) {
-				embd.addField("Continuação", "```js\n" + result.slice(1010, 2020) + "```")
+		let result = eval(args.join(' '));
+		let simple = eval(args.join(' '))
+		const filter = i => i.user.id === message.author.id;
+		const collector = message.channel.createMessageComponentCollector({ filter, time: 15000 });
+
+		message.reply({ embeds: [stageone], components: [row] }).then(async message => {
+			if (!args[0]) {
+				return message.edit(`Insira um valor para executar o eval.`);
 			}
-			if (result.length > 2020) {
-				embd.addField("Continuação", "```js\n" + result.slice(1010, 2020) + "```")
+			try {
+				let beforeRunning = Date.now(); // Define a data de execução
+				if (result instanceof Promise) {
+					await message.edit('O código retornou uma promise - aguardando ela ser resolvida...')
+					await result
+					await simple
+				}
+				if (typeof result !== 'string') result = require('util').inspect(result);
+				let end = (Date.now() - beforeRunning); // Define o final do tempo
+
+				let simpler = new Embed(u)
+				simpler.addField("📥 Entrada", "```js\n" + args.join(' ') + "```")
+				simpler.addField("📩 Saída", "```js\n" + result.slice(0, 1010) + "```")
+				simpler.setFooter({
+					text: `Time: ${end}ms`,
+					IconURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAY24-TSTqg2C9tlU2TpNpRnLW11kx8m4Mzw&usqp=CAU'
+				})
+
+				let embd = new Embed(u)
+				embd.addField("📥 Entrada", "```js\n" + args.join(' ') + "```")
+				embd.addField("📩 Saída", "```js\n" + result.slice(0, 1010) + "```")
+				embd.setFooter({
+					text: `Time: ${end}ms`,
+					IconURL: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAY24-TSTqg2C9tlU2TpNpRnLW11kx8m4Mzw&usqp=CAU'
+				})
+				if (result.length > 1010) {
+					embd.addField("Continuação", "```js\n" + result.slice(1010, 2020) + "```")
+					simpler.addField("Continuação", "```js\n" + result.slice(1010, 2020) + "```")
+				}
+				if (result.length > 2020) {
+					embd.addField("Continuação", "```js\n" + result.slice(1010, 2020) + "```")
+					simpler.addField("Continuação", "```js\n" + result.slice(1010, 2020) + "```")
+
+				}
+				if (result.length > 3030) {
+					embd.addField("Continuação", "```js\n" + result.slice(2020, 3030) + "```")
+				}
+				collector.on('collect', async i => {
+					if (i.customId === 'lup') {
+							await i.update({ embeds: [simpler], components: [] })
+
+					}
+					if (i.customId === 'pc') {
+						await i.update({ embeds: [embd], components: [] })
+					}
+				})
+
+			} catch (e) {
+				message.edit({ content: 'Toma aqui teu erro seu filho da puta\n```js\n' + e + '```' })
 			}
-			if (result.length > 3030) {
-				embd.addField("Continuação", "```js\n" + result.slice(2020, 3030) + "```")
-			}
-			message.reply({ embeds: [embd] })
-		} catch (e) {
-			message.edit({ content: 'Toma aqui teu erro seu filho da puta\n```js\n' + e + '```' })
-		}
+		})
 	}
 })
