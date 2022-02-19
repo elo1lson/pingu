5a62a501ed9d6b7d18af2cc153d09369f564254f
elo1lson <eloilsonfontenele2@gmail.com>
2022-02-19 18:19:05 -0300

    r

--- src/commands/owner/CommandServerList.js
+++ src/commands/owner/CommandServerList.js
@@ -2,6 +2,7 @@ const Command = require('../../structures/command/command.js')
 const Embed = require('../../structures/client/ClientEmbed.js')
 module.exports = new Command({
 	name: 'topservers',
+	aliases: ['tops'],
 	run: async (client, message, args) => {
 		if(message.author.id != '539945189901336586') return	
 		let guild = client.guilds.cache.sort((a, b) => b.memberCount - a.memberCount).first(30);
