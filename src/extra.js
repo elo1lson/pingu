b1a5a95245e6d6a7efbb581759c24a2433db4b4e
bobycake <81685769+elo1lson@users.noreply.github.com>
2022-02-18 21:13:43 -0300

    Update extra.js

--- src/extra.js
+++ src/extra.js
@@ -1,4 +1,15 @@
-const extra = {
-	helper: require('./locales/pt-Br/helper.json')
+const locales = {
+	run = {
+		BOTINFO: require('./locales/pt-Br/run/botinfo.json'),
+		HELP: require('./locales/pt-Br/run/help.json'),
+		INVITE: require('./locales/pt-Br/run/help.json')
+	}, 
+	descriptions = {
+		BOTINFO: require('./locales/pt-Br/descriptions/botinfo.json'),
+		HELP: require('./locales/pt-Br/descriptions/help.json'),
+		INVITE: require('./locales/pt-Br/descriptions/invite.json')
+	}
+	//helper: require('./locales/pt-Br/helper.json')
 }
-module.exports = extra
+module.exports = locales
+
