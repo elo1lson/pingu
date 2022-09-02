6c7ce531a2448b548be4c6bba819c27723746953
elo1lson <eloilsonfontenele2@gmail.com>
2022-09-01 21:25:28 -0300

    feat: add base for locales module

--- packages/locale/resolve.js
+++ packages/locale/resolve.js
@@ -1,7 +1,30 @@
 'use strict'
+import yaml from "yaml"
+import fs from "fs"
 
-export default class Resolve{
-    constructor(lang){
+import { deflateSync } from "zlib"
+export default class Resolve {
+    constructor(lang) {
+        this.load()
+        this.langs = []
+
+    }
+    load() {
+        let temporaryLangs []
+
+        fs.readdirSync(path).forEach(folder => {
+            temporaryLangs.push(folder)
+
+        })
+
+    }
+    get traduce() {
+
+    }
+    get messages() {
+
+    }
+    get error() {
 
     }
 }
